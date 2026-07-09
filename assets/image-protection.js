459a363bbdf881422b717fa6e74b047ac812d36f
Anonymous <devyasir001@gmail.com>
2026-07-09 16:23:44 +0500

    do

--- assets/image-protection.js
+++ assets/image-protection.js
@@ -1,22 +1,19 @@
 (function () {
   'use strict';
 
-  var IMAGE_ZONE_SELECTOR =
-    'img, picture, .card-product-tabs__media, .product__media, .product__media-wrapper, swiper-gallery, media-gallery, .product-media-container';
-
-  function isInImageZone(target) {
+  function isImage(target) {
     if (!(target instanceof Element)) return false;
-    return Boolean(target.closest(IMAGE_ZONE_SELECTOR));
+    return target instanceof HTMLImageElement || Boolean(target.closest('picture'));
   }
 
   document.addEventListener('contextmenu', function (event) {
-    if (isInImageZone(event.target)) {
+    if (isImage(event.target)) {
       event.preventDefault();
     }
   });
 
   document.addEventListener('dragstart', function (event) {
-    if (isInImageZone(event.target)) {
+    if (isImage(event.target)) {
       event.preventDefault();
     }
   });
